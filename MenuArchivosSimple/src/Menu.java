fd0f60f0415a7b0d8775c29dc30c4c0c00127b8f
Hanga7yr <37352386+Hanga7yr@users.noreply.github.com>
2020-10-06 12:05:48 +0200

    Finalización del menu
    Implementación de todas las funcionalidades del menu.

--- MenuArchivosSimple/src/Menu.java
+++ MenuArchivosSimple/src/Menu.java
@@ -1,3 +1,4 @@
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public abstract class Menu {
@@ -20,7 +21,7 @@ public abstract class Menu {
 			for(String i : lineas)
 				System.out.println(i);
 			
-			System.out.print("/nIngrese la opción a realizar: ");
+			System.out.print("\nIngrese la opción a realizar: ");
 			String entrada = input.nextLine();
 					
 			try {
@@ -32,12 +33,16 @@ public abstract class Menu {
 			
 			switch(option) {
 				case 1:
+					copyFiles();
 					break;
 				case 2:
+					createFile();
 					break;
 				case 3:
+					deleteFile();
 					break;
 				case 4:
+					showFile();
 					break;
 				case 5:
 					salir = true;
@@ -53,8 +58,53 @@ public abstract class Menu {
 		String origin = "";
 		String dest = "";
 		
-		System.out.print("Introduce el fichero de origen: ");
+		System.out.print("Introduce la ruta del fichero de origen: ");
 		origin = input.nextLine();
 		
+		System.out.print("Introduce la ruta del fichero de destino: ");
+		dest = input.nextLine();
+		
+		if(!Fichero.CopiarFichero(origin, dest))	System.out.println("Error en la copia de los ficheros.");
+		else										System.out.println("Se han copiado los ficheros correctamente.");
 	}
+	
+	public static void createFile() {
+		String file = "";
+		
+		System.out.print("Introduce la ruta del fichero a crear: ");
+		file = input.nextLine();
+		
+		if(!Fichero.CrearFichero(file))	System.out.println("Error en la creación del fichero.");
+			
+	}
+	
+	public static void deleteFile() {
+		String file = "";
+		
+		System.out.print("Introduce la ruta del fichero a eliminar: ");
+		file = input.nextLine();
+		
+		if(!Fichero.EliminarFichero(file)) {
+			System.out.println("Error en el borrado del fichero.");
+		}
+	}
+	
+	public static void showFile() {
+		String file = "";
+		ArrayList<String> content = null;
+		
+		System.out.print("Introduce la ruta del fichero a leer: ");
+		file = input.nextLine();
+		
+		if((content = Fichero.LeerFichero(file)) == null) {
+			System.out.println("Error en la lectura del fichero.");
+		}else {
+			System.out.println();
+			
+			for(String i : content)
+				System.out.println(i);
+			
+			System.out.println();
+		}
+}
 }
